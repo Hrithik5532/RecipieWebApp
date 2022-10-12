cd027d1d30a50afbc4400a10634ad22d483639b3
Hrithik <hrithikhadawale75@gmail.com>
2022-10-13 02:49:25 +0530

    efbie

--- videoApp/settings.py
+++ videoApp/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -122,7 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT='static'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'Templates/static')]
+
 MEDIA_URL='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 # Default primary key field type
