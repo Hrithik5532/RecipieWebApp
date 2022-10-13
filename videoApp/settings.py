5f759f5d0658e93878b798dfa00cc22a3a3b8eb9
Hrithik <hrithikhadawale75@gmail.com>
2022-10-14 01:10:12 +0530

    wkhkf

--- videoApp/settings.py
+++ videoApp/settings.py
@@ -52,6 +52,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+        "whitenoise.middleware.WhiteNoiseMiddleware",
+
 ]
 
 ROOT_URLCONF = 'videoApp.urls'
@@ -123,8 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT='static'
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'Templates/static')]
+STATIC_ROOT=os.path.join(BASE_DIR,'Templates/static')
+# STATICFILES_DIRS = [os.path.join(BASE_DIR,'Templates/static')]
 
 MEDIA_URL='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
